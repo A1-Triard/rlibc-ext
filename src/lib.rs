5ad8917a99ad184ab7807f93b020bcde584a416e
Warlock <internalmike@gmail.com>
2024-09-19 05:32:36 +0300

    atomics

--- src/lib.rs
+++ src/lib.rs
@@ -51,3 +51,117 @@ pub unsafe extern "C" fn _aulldiv(a: c_ulonglong, b: c_ulonglong) -> c_ulonglong
 pub unsafe extern "C" fn _aullrem(a: c_ulonglong, b: c_ulonglong) -> c_ulonglong {
     a % b
 }
+
+#[cfg(any(target_os="dos", docsrs))]
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+unsafe extern "C" fn __atomic_load(src: *const usize, _model: c_int) -> usize {
+    asm!("cli");
+    let dest = *src;
+    asm!("sti", "nop");
+    dest
+}
+
+#[cfg(any(target_os="dos", docsrs))]
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+unsafe extern "C" fn __atomic_store(dest: *mut usize, src: usize, _model: c_int) {
+    asm!("cli");
+    *dest = src;
+    asm!("sti", "nop");
+}
+
+#[cfg(any(target_os="dos", docsrs))]
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+unsafe extern "C" fn __atomic_load_1(src: *const u8, _model: c_int) -> u8 {
+    asm!("cli");
+    let dest = *src;
+    asm!("sti", "nop");
+    dest
+}
+
+#[cfg(any(target_os="dos", docsrs))]
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+unsafe extern "C" fn __atomic_store_1(dest: *mut u8, src: u8, _model: c_int) {
+    asm!("cli");
+    *dest = src;
+    asm!("sti", "nop");
+}
+
+#[cfg(any(target_os="dos", docsrs))]
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+unsafe extern "C" fn __atomic_load_2(src: *const u16, _model: c_int) -> u16 {
+    asm!("cli");
+    let dest = *src;
+    asm!("sti", "nop");
+    dest
+}
+
+#[cfg(any(target_os="dos", docsrs))]
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+unsafe extern "C" fn __atomic_store_2(dest: *mut u16, src: u16, _model: c_int) {
+    asm!("cli");
+    *dest = src;
+    asm!("sti", "nop");
+}
+
+#[cfg(any(target_os="dos", docsrs))]
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+unsafe extern "C" fn __atomic_load_4(src: *const u32, _model: c_int) -> u32 {
+    asm!("cli");
+    let dest = *src;
+    asm!("sti", "nop");
+    dest
+}
+
+#[cfg(any(target_os="dos", docsrs))]
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+unsafe extern "C" fn __atomic_store_4(dest: *mut u32, src: u32, _model: c_int) {
+    asm!("cli");
+    *dest = src;
+    asm!("sti", "nop");
+}
+
+#[cfg(any(target_os="dos", docsrs))]
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+unsafe extern "C" fn __atomic_load_8(src: *const u64, _model: c_int) -> u64 {
+    asm!("cli");
+    let dest = *src;
+    asm!("sti", "nop");
+    dest
+}
+
+#[cfg(any(target_os="dos", docsrs))]
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+unsafe extern "C" fn __atomic_store_8(dest: *mut u64, src: u64, _model: c_int) {
+    asm!("cli");
+    *dest = src;
+    asm!("sti", "nop");
+}
+
+#[cfg(any(target_os="dos", docsrs))]
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+unsafe extern "C" fn __atomic_load_16(src: *const u128, _model: c_int) -> u128 {
+    asm!("cli");
+    let dest = *src;
+    asm!("sti", "nop");
+    dest
+}
+
+#[cfg(any(target_os="dos", docsrs))]
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+unsafe extern "C" fn __atomic_store_16(dest: *mut u128, src: u128, _model: c_int) {
+    asm!("cli");
+    *dest = src;
+    asm!("sti", "nop");
+}
